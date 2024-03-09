7aeb80b4fb0b38e72588179d61f2f7b258b25e48
foehammer127 <foehammer@disroot.org>
2024-03-09 16:52:53 -0600

    feat(cmd/default.nix): add docker image for the newsletter cron service
    Add a deployable docker image for the newsletter cron service.

--- cmd/default.nix
+++ cmd/default.nix
@@ -1,9 +1,18 @@
 { config, self', inputs', pkgs, system, ... }:
-{
+rec {
   packages.cron = pkgs.buildGoApplication {
     name = "cron";
     src = ../.;
     modules = ../gomod2nix.toml;
     subpackages = [ "cmd/cron" ];
+  };
+
+  packages.cron-docker = pkgs.dockerTools.buildLayeredImage {
+    name = "newsletter/cron";
+    tag = "latest";
+    config = {
+      Env = [ "PRODUCTION=true" ];
+      Cmd = [ "${packages.cron}/bin/cron" ];
+    };
   };
 }
