3ebd670e6ab77e0a73f98332a93a5136d1239b0f
foehammer127 <foehammer@disroot.org>
2024-03-09 19:44:19 -0600

    feat(lib/common): add deploy public key as member of root deployers

--- lib/common/default.nix
+++ lib/common/default.nix
@@ -28,7 +28,11 @@ with lib; {
 
     users.users.root = mkIf cfg.sshRootLogin {
       openssh.authorizedKeys.keys = [
+        # Foehammer
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICz6aGtthhHrVYHrvk3BzCJTRFb3ppRB2MjHGI+eFteG foehammer127@gmail.com"
+
+        # Github actions runners.
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICn76lECi6dMUiz5nLnZLDLbFPSnDRoRr7gQfkH83xEQ stc@spa.edu"
       ];
     };
   };
